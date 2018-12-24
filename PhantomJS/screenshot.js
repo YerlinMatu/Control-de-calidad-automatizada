e315584ba35d53d3692f297cec4cca4b502ee130
Yerlin Matu <yerlinmatu@MacBook-Pro-de-Yerlin.local>
2018-12-24 14:49:34 -0500

    Fixed tabulation

---
+++ PhantomJS/screenshot.js
@@ -0,0 +1,7 @@
+const page = require('webpage').create();
+page.open('http://www.google.com', function() {
+    setTimeout(function() {
+        page.render('google.png');
+        phantom.exit();
+    }, 200);
+});
